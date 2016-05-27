1e9d80c3fa1329842ff2ffc702431c838b07b54d
Kert <kert.luo@gmail.com>
2016-05-27 15:45:50 +0800

    add ssl listening and an upload logic to test AUT

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
 
 var app = express();
 
@@ -11,8 +13,25 @@ app.use(function (req, res, next) {
 });
 //app.use('/', express.static(staticPath));
 app.use(express.static('public'));
+app.post('/upload', function(req, res){
+    var total = '';
+//    res.send('POST a file.');
+    req.on('data', function (data) {
+        total+=data;
+    });
+    req.on('end', function(){
+        //console.log(total.toString());
+        res.send(total.length.toString());
 
+    })
+})
 app.listen(3000, function() {
     console.log('listening');
 });
+var privateKey  = fs.readFileSync('public/sslcert/cert.cer', 'utf8');
+var certificate = fs.readFileSync('public/sslcert/cert.cer', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials,app);
+httpsServer.listen(3443);
 
